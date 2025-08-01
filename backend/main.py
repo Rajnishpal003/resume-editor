b13ffa5417579cf3b3c8558bd9da936b33c38a68
Rajnishpal003 <Rajnishpal006@gmail.com>
2025-08-01 16:54:22 +0530

    f

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,9 @@ app = FastAPI()
 # Enable CORS for frontend connection
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173",  # local dev
+        "https://resume-editor-lac.vercel.app",  # ✅ your fixed production domain
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
